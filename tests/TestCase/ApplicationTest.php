9b69a01bf1850458a00ff28d823e512ad916a5ad
Nathan Cyr <nathan.cyr@gmail.com>
2018-11-13 12:24:02 -0500

    fix de quelques tests

--- tests/TestCase/ApplicationTest.php
+++ tests/TestCase/ApplicationTest.php
@@ -40,11 +40,13 @@ class ApplicationTest extends IntegrationTestCase
         $app->bootstrap();
         $plugins = $app->getPlugins();
 
-        $this->assertCount(4, $plugins);
+        $this->assertCount(6, $plugins);
         $this->assertSame('Bake', $plugins->get('Bake')->getName());
         $this->assertSame('Migrations', $plugins->get('Migrations')->getName());
         $this->assertSame('DebugKit', $plugins->get('DebugKit')->getName());
         $this->assertSame('Crud', $plugins->get('Crud')->getName());
+        $this->assertSame('BootstrapUI', $plugins->get('BootstrapUI')->getName());
+        $this->assertSame('CakePdf', $plugins->get('CakePdf')->getName());
     }
 
     /**
